3f5c46aa9576b8207b3893c55af9ea6ffc41bfc8
Emran Rastadi <e.rastadi@hesaba.co>
2025-12-29 20:29:12 +0330

    feat: @ctx add to controller

--- apps/product-service/src/app/product.consumer.ts
+++ apps/product-service/src/app/product.consumer.ts
@@ -1,5 +1,10 @@
 import { Controller, Logger } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  KafkaContext,
+  Payload,
+} from '@nestjs/microservices';
 import { Events, type OrderCreatedPayload } from '@microservice/contracts';
 
 @Controller()
@@ -7,11 +12,14 @@ export class ProductConsumer {
   private readonly logger = new Logger(ProductConsumer.name);
 
   @EventPattern(Events.ORDER_CREATED)
-  handle(@Payload() orderDto: OrderCreatedPayload) {
+  handle(
+    @Payload() orderDto: OrderCreatedPayload,
+    @Ctx() context: KafkaContext,
+  ) {
     this.logger.log(
       `Received Order Created event: ${JSON.stringify(orderDto)}`,
     );
-    console.log(`Received Order Created event`);
+    this.logger.log(`Context Of Event: ${JSON.stringify(context)}`);
     // TODO: business logic, e.g., update inventory
   }
 }
